a060379ceb4a9968784ec01c321c2f6efb8cabba
julius <nyerereoffice@gmail.com>
2024-05-06 00:56:32 +0300

    Prisma Configuration

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,5 @@
 import { Controller, Get, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
-
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
