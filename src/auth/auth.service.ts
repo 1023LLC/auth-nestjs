ac3770f227cdd9612cd661f1bb1d8d69bcd2b661
julius <nyerereoffice@gmail.com>
2024-05-06 06:13:22 +0300

    Sign Up

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,32 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { AuthDto } from './dto/auth.dto';
+import { PrismaService } from 'prisma/prisma.service';
+
+import * as bcrypt from 'bcrypt';
+
 
 @Injectable()
 export class AuthService {
-    constructor() {}
+    constructor(private prisma: PrismaService) {}
 
-    async signup() {
+    async signup(dto:AuthDto) {
+        const { email, password } = dto;
+
+        const foundUser = await this.prisma.user.findUnique({where: {email}})
+
+        if (foundUser){
+            throw new BadRequestException('Email already exits!')
+        }
+
+        const hashedPassword = await this.hashPassword(password)
+
+        await this.prisma.user.create({
+            data: {
+                email,
+                hashedPassword
+            }
+        })
+
         return { message: 'Sign up was successful' }
     }
 
@@ -16,5 +38,14 @@ export class AuthService {
 
     async signout() {
         return ''
+    }
+
+
+    async hashPassword(password: string){
+        const saltOrRounds = 10;
+
+        const hashedPassword = await bcrypt.hash(password, saltOrRounds);
+
+        return hashedPassword
     }
 }
---
+++ src/auth/dto/auth.dto.ts
@@ -0,0 +1,14 @@
+import { IsEmail, IsNotEmpty, IsString, Length } from "class-validator";
+
+
+
+export class AuthDto {
+    @IsEmail()
+    public email: string;
+
+
+    @IsNotEmpty()
+    @IsString()
+    @Length(3, 20, {  message: 'Password to be between 3, 20 characters'})
+    public password: string
+}
