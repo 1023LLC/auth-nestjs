c134b57ae31fc495fa087ca88b392917b27ca997
julius <nyerereoffice@gmail.com>
2024-05-06 11:24:42 +0300

    Users Resource

---
+++ src/users/users.service.ts
@@ -0,0 +1,34 @@
+/* eslint-disable prettier/prettier */
+
+import { ForbiddenException, Injectable, NotFoundException } from '@nestjs/common';
+import { Request } from 'express';
+import { PrismaService } from 'prisma/prisma.service';
+
+@Injectable()
+export class UsersService {
+    constructor(private prisma: PrismaService) {}
+
+
+    async getMyUser(id: string, req: Request){
+        const user = await this.prisma.user.findUnique({ where: {id} })
+
+        if (!user){
+            throw new NotFoundException()
+        }
+
+        const decodedUser = req.user as {id:string, email:string}
+
+        if (user.id !== decodedUser.id){
+            throw new ForbiddenException({ message: "Unauthorized access!" })
+        }
+
+        delete user.hashedPassword;
+
+        return { user }
+    }
+
+
+    async getUsers(){
+        return await this.prisma.user.findMany({select: {id: true, email: true}})
+    }
+}
